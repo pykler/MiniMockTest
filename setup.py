3e0745f40efd53e6bb67db885d4e5fe749ad9e93
Hatem Nassrat <hnassrat@gmail.com>
2011-08-12 17:43:42 -0300

    Adding a setup file for the module.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import sys, os
+
+setup(name='minimocktest',
+      version='0.1',
+      description="Custom unittest TestCase that wraps minimock",
+      long_description="""\
+Custom unittest TestCase that wraps minimock for easier use """,
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='minimock mock mocking test unittest TestCase MockTestCase',
+      author='Hatem Nassrat',
+      author_email='hnassrat@gmail.com',
+      url='http://pykler.github.com/MiniMockTest/',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
